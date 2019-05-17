67357b58185be8410f5b267fbeac3f49486372f8
Martín Stancanelli <mastanca14@gmail.com>
2019-05-17 19:49:39 -0300

    Conform ERC20 interface

--- Pether.sol
+++ Pether.sol
@@ -13,6 +13,7 @@ contract Pether is ERC20Interface, Owned {
     uint _totalSupply;
 
     mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
 
     constructor() public {
         symbol = "PET";
@@ -22,4 +23,38 @@ contract Pether is ERC20Interface, Owned {
         balances[owner] = _totalSupply;
         emit Transfer(address(0), owner, _totalSupply);
     }
+
+    function totalSupply() public view returns (uint) {
+        return _totalSupply.sub(balances[address(0)]);
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = balances[from].sub(tokens);
+        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
 }
