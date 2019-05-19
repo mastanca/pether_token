1789e4853353fdc9c68ada063cd56db261676bca
Martín Stancanelli <mastanca14@gmail.com>
2019-05-18 22:35:47 -0300

    Implement future token buying

--- ROFEXStyle.sol
+++ ROFEXStyle.sol
@@ -1,4 +1,6 @@
 pragma solidity ^0.5.8;
+// To return array of futureTransactions to client
+pragma experimental ABIEncoderV2;
 
 import "./Owned.sol";
 import "./SafeMath.sol";
@@ -11,17 +13,29 @@ contract ROFEXStyle is Owned {
         uint price;
     }
 
+    struct FutureTransaction {
+        uint price;
+        uint qty;
+        uint date;
+        bool ready;
+        address buyer;
+    }
+
     uint transactionCount;
     uint m;
     uint b;
     Transaction[] lastTransactions;
+    FutureTransaction[] futureTransactions;
 
     constructor() public {
         transactionCount = 0;
     }
 
     function ejecutarRegresion() public {
-        require(transactionCount >= 4);
+        require(
+            transactionCount >= 4,
+            "Not enough transactions"
+            );
         // y is price, t is dateCreated
         // y = b+m*t
         uint t = 0;
@@ -39,28 +53,66 @@ contract ROFEXStyle is Owned {
         b = y.sub((b.mul(t))).div(lastFourTransactions.length);
     }
 
-    function calcularValorFuturo(uint fecha) public view {
-
+    function calcularValorFuturo(uint fecha) public view returns (uint) {
+        // TODO: Implement me
     }
 
     function comprarMonedaFutura(uint fecha, uint cantidad) public payable {
-
+        require(
+            fecha < now.add(90 days),
+            "Can't buy more than 90 days ahead"
+            );
+        require(
+            fecha > now,
+            "Parameter 'fecha' must be in the future"
+        );
+        uint futurePrice = calcularValorFuturo(fecha);
+        futureTransactions.push(FutureTransaction(futurePrice, cantidad, fecha, false, msg.sender));
     }
 
-    function consultarMisComprasFuturas() public view {
+    function consultarMisComprasFuturas() public view returns (FutureTransaction[] memory){
+        FutureTransaction[] memory futures = getFuturesFor(msg.sender);
+        for (uint index = 0; index < futures.length; index++) {
+            if (futures[index].date > now) {
+                futures[index].ready = true;
+            }
+        }
+        return futures;
+    }
 
+    /**
+        Ok so, solidity doesn't allow us to return dynamic sized arrays
+        we have to loop the first time to get the actual size of the returned array
+        and then another time to build it.
+        Also there's is no push for local arrays, so we need the reset of count.
+     */
+    function getFuturesFor(address buyer) private view returns (FutureTransaction[] memory) {
+        uint count = 0;
+        for (uint index = 0; index < futureTransactions.length; index++) {
+            if (futureTransactions[index].buyer == buyer) {
+                count++;
+            }
+        }
+        count = 0;
+        FutureTransaction[] memory futures = new FutureTransaction[](count);
+        for (uint index = 0; index < futureTransactions.length; index++) {
+            if (futureTransactions[index].buyer == buyer) {
+                futures[0] = futureTransactions[index];
+            }
+        }
+        return futures;
     }
 
     function consultarTodasLasComprasFuturas() public view onlyOwner {
-
+        // TODO: Implement me
     }
 
     function ejecutarMisContratos() public {
-
+        // TODO: Implement me
     }
 
     function ejecutarTodosLosContratos() public onlyOwner {
-
+        // TODO: Implement me
     }
 
     function recordNewTransaction(uint price) internal {
